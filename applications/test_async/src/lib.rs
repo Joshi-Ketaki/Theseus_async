7c4caf744ea5c7ee6fb452e6db46f045c58ee2c2
Caihua Li <caihuali95@gmail.com>
2020-11-29 17:23:14 -0500

    Add test_async application to test async task & executor

---
+++ applications/test_async/src/lib.rs
@@ -0,0 +1,32 @@
+#![no_std]
+
+extern crate alloc;
+extern crate task_async;
+#[macro_use] extern crate terminal_print;
+
+use alloc::string::String;
+use task_async::AsyncTask;
+use task_async::async_executor::AsyncExecutor;
+
+async fn test() -> String {
+    String::from("Hello")
+}
+
+async fn print() {
+    let msg = test().await;
+    println!("message = {}", msg);
+}
+
+pub fn main() -> isize {
+    println!("test_async.");
+
+    let mut executor = AsyncExecutor::new();
+    let task = AsyncTask::new(print());
+    executor.spawn(task);
+
+    println!("Ready to run executor.");
+    executor.run();
+
+
+    0
+}
